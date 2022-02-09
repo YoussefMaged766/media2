eecbe84e8096924a343bb1d759fb5f1a8292a48f
YoussefMaged766 <yoer766@gmail.com>
2022-02-09 02:39:26 +0200

    v1

--- app/src/main/java/com/example/media2/MainActivity.kt
+++ app/src/main/java/com/example/media2/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.media2
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+printHashKey(applicationContext)
+
+    }
+    private fun printHashKey(context: Context) {
+
+        try {
+            val info = context.packageManager.getPackageInfo(
+                context.packageName,
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("AppLog", "key:$hashKey=")
+            }
+        } catch (e: Exception) {
+            Log.e("AppLog", "error:", e)
+        }
+
     }
 }
